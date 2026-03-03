9b9b8a83f0f1bffb015c38f7c98c6e93ff1f4681
418teapot <wangshuo2912@foxmail.com>
2026-03-03 15:13:42 +0800

    feat(config): add git and neovim config

--- home/packages.nix
+++ home/packages.nix
@@ -19,10 +19,27 @@
     zellij
   ];
 
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "wangshuo2";
+        email = "wangshuo2@cambricon.com";
+      };
+    };
+  };
   programs.home-manager.enable = true;
   programs.neovim = {
     enable = true;
     vimAlias = true;
     vimdiffAlias = true;
+  };
+
+  home.file.".config/nvim" = {
+    source = builtins.fetchGit {
+      url = "https://github.com/418-teapot/NeoDot.git";
+      ref = "master";
+      rev = "0e66c0eae734e6d764e81821090151517dc4eef9";
+    };
   };
 }
